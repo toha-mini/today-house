9e866b1fe54431a65376829dbeac9d2d75735b71
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-07-24 13:06:21 +0900

    fix.header revotory

---
+++ src/features/header/test.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const test = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam cum quidem, dolorum corrupti, perspiciatis
+			voluptatum ab consequuntur expedita, incidunt fuga accusamus. Qui et, delectus ab, omnis suscipit blanditiis
+			id odit magni debitis esse nam enim nihil facere sit. Dolores omnis pariatur dolor error cumque? Eveniet
+			reprehenderit optio, accusantium eius harum culpa iure officiis nemo voluptate dolores nam recusandae
+			consequuntur, ipsam dolore. Ab omnis quis nisi doloremque, molestias inventore. Autem ducimus repudiandae
+			tempore nisi dicta distinctio blanditiis qui vel dolorum ab velit, pariatur sit corporis, quo veniam.
+			Impedit nesciunt unde minus veritatis porro consequuntur autem labore eaque, quisquam ipsa repudiandae amet
+			expedita rem ea dolor aliquam asperiores iusto voluptate quae commodi odit quasi dolores? Minus sunt, vero a
+			repellendus explicabo laboriosam unde error enim vitae consectetur labore nemo iure, eaque, nam esse ut
+			molestiae placeat quam possimus odio? Eligendi tempora nemo sint veritatis et quis totam incidunt
+			repellendus impedit consequuntur? Doloremque dolores harum repellendus ad accusantium voluptates quam quasi
+			autem, odit quisquam illum quo deserunt beatae nesciunt odio voluptas nihil rerum, et debitis voluptate ipsa
+			a impedit. Culpa minima aliquid, a, impedit odio corporis odit ipsum commodi, laudantium adipisci maiores id
+			qui sit aspernatur amet ab ex. Quae, qui! Officia ducimus nihil nulla praesentium velit incidunt autem
+			cumque saepe. Quia assumenda impedit dicta, nemo quis accusamus officiis distinctio consequuntur ipsum quo
+			iure unde aliquid fuga enim natus eligendi ab odit sed soluta sunt? Eos consequatur consectetur doloremque
+			repellat pariatur! Consectetur sunt at aperiam enim natus, amet iusto minus fugit fuga, vero unde voluptas
+			neque commodi vitae expedita nesciunt, veniam facilis architecto dignissimos totam? Perferendis iusto
+			voluptas dolorem nihil commodi error earum recusandae repellat reiciendis distinctio voluptatem quos
+			praesentium deleniti et quibusdam similique accusamus, autem necessitatibus. Non placeat inventore, corrupti
+			tempore veritatis sint repudiandae nesciunt atque eum, velit dolore doloremque impedit eius?
+		</div>
+	);
+};
+
+export default test;
