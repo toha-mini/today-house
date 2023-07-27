29706b13c7aa41e388fd6af128ff29e0e8418f2f
seo myeong jin <seomyeongjin99@gmail.com>
2023-07-27 17:51:19 +0900

    Feat : 회원가입 페이지 css 수정

--- src/features/account/AgreementCheckBox.jsx
+++ src/features/account/AgreementCheckBox.jsx
@@ -138,16 +138,22 @@ const AreeTitleContainer = styled.div`
   align-items: center;
 `;
 
-// const CheckInput1 = styled.input`
-//   accent-color: red;
-// `;
 const CheckInput = styled.input`
+  appearance: none;
   border: 1px solid #2f3438;
   border-radius: 4px;
   width: 18px;
   height: 18px;
   margin-right: 5px;
-  accent-color: rgb(53, 197, 240);
+
+  &:checked {
+    border-color: transparent;
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
+    background-size: 100% 100%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    background-color: #35c5f0;
+  }
 `;
 
 const AgreeContents = styled.span`
